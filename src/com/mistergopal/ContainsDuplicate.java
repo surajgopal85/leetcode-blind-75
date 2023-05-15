64c56845693b43937c8e4113e41bc3eecf723067
Suraj Gopal <surajcancode@gmail.com>
2023-05-15 10:31:00 -0400

    contains duplicate - sort first, 1 pass comparison

---
+++ src/com/mistergopal/ContainsDuplicate.java
@@ -0,0 +1,19 @@
+package com.mistergopal;
+
+import java.util.Arrays;
+
+public class ContainsDuplicate {
+    public boolean solution(int[] nums) {
+        // problem, return true if nums contains any duplicate values, false if all uniques
+        // solution: sort arr first to avoid complicated comparisons
+        // adjust break condition to compare 1 ahead of i 
+
+        Arrays.sort(nums);
+        for(int i = 0; i < nums.length - 1; i++) {
+            if(nums[i] == nums[i + 1]) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
