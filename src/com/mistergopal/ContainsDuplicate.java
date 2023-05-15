ac88b54f3fe45cf39753b2d7d6d3e20ec7e51c78
Suraj Gopal <surajcancode@gmail.com>
2023-05-15 10:31:28 -0400

    note runtime containsDups

--- src/com/mistergopal/ContainsDuplicate.java
+++ src/com/mistergopal/ContainsDuplicate.java
@@ -5,8 +5,9 @@ import java.util.Arrays;
 public class ContainsDuplicate {
     public boolean solution(int[] nums) {
         // problem, return true if nums contains any duplicate values, false if all uniques
+        // O(n)
         // solution: sort arr first to avoid complicated comparisons
-        // adjust break condition to compare 1 ahead of i 
+        // adjust break condition to compare 1 ahead of i
 
         Arrays.sort(nums);
         for(int i = 0; i < nums.length - 1; i++) {
