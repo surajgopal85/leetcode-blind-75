195d6e181b063edca801e39e554e2383bdc17a82
Suraj Gopal <surajcancode@gmail.com>
2023-05-15 09:05:29 -0400

    added comments to remove dups

--- src/com/mistergopal/RemoveDuplicates.java
+++ src/com/mistergopal/RemoveDuplicates.java
@@ -2,6 +2,12 @@ package com.mistergopal;
 
 public class RemoveDuplicates {
     public int removeDups(int[] nums) {
+        // start i counter at beginning of array
+        // j counter begins after
+        // every time contents of i and j do not match, you have a unique element:
+        // increment i, reset nums[i] to the next element (nums[j])
+        // since this is a "late counter" (increments after unique found) it must be returned + 1
+        // to capture the true # of unique elements
         int i = 0;
         for(int j = 1; j < nums.length; j++) {
             if(nums[i] != nums[j]) {
